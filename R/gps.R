96276c521219d83e69f1194850a2b27563f6728b
ashirwad <ashirwad1992@gmail.com>
2022-10-22 23:18:40 -0500

    Replace check_* funs with assert_* counterparts

--- R/gps.R
+++ R/gps.R
@@ -6,11 +6,11 @@ set_gps_params <- function(file,
                            y = "y",
                            timestamp = "timestamp") {
   # Error handling
-  checkmate::check_string(file)
-  checkmate::check_string(id)
-  checkmate::check_string(geom)
-  checkmate::check_flag(gps_point)
-  checkmate::check_string(x)
-  checkmate::check_string(y)
-  checkmate::check_string(timestamp)
+  checkmate::assert_string(file)
+  checkmate::assert_string(id)
+  checkmate::assert_string(geom)
+  checkmate::assert_flag(gps_point)
+  checkmate::assert_string(x)
+  checkmate::assert_string(y)
+  checkmate::assert_string(timestamp)
 }
