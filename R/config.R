74f41e00c9520ac453396f050b668832b170197e
ashirwad <ashirwad1992@gmail.com>
2022-10-23 20:42:59 -0500

    Document set_gps_params() function

--- R/config.R
+++ R/config.R
@@ -1,3 +1,17 @@
+#' Set GPS parameters
+#'
+#' @param file GPS file name.
+#' @param id GPS id column name.
+#' @param geom GPS geometry column name. Only applicable to GPS trajectory CSV file.
+#' @param gps_point If specified read input data as GPS point, otherwise read input data as trajectory.
+#' @param x GPS x column name. Only applicable to GPS point CSV file.
+#' @param y GPS y column name. Only applicable to GPS point CSV file.
+#' @param timestamp GPS timestamp column name.
+#'
+#' @return A list.
+#' @export
+#'
+#' @examples
 set_gps_params <- function(file,
                            id = "id",
                            geom = "geom",
